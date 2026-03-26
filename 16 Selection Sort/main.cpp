8391a3fb2617b44ab33d734fcd30ea9b70f9db9f
Ravindra Yadav <ravindray.dev@gmail.com>
2026-03-26 19:01:50 +0530

    Add 16 Selection Sort

---
+++ 16 Selection Sort/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void selectionSort(vector<int> &arr, int n) {
+  for (int i = 0; i < n - 1; i++) {
+    int minIndex = i;
+
+    for (int j = i + 1; j < n; j++) {
+      if (arr[j] < arr[minIndex]) {
+        minIndex = j;
+      }
+    }
+    swap(arr[minIndex], arr[i]);
+  }
+}
+
+int main() {
+  vector<int> arr = {29, 72, 98, 13, 87, 66, 52, 51, 36};
+
+  selectionSort(arr, arr.size());
+
+  for (int val : arr) {
+    cout << val << " ";
+  }
+  return 0;
+}
